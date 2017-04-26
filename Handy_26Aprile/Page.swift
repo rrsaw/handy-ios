a58e626421492773dac4d2e8bb0e8a628f0de37f
Fede <federicocattaneo1@ied.edu>
2017-04-27 00:48:07 +0200

    Intro base
    Creazione del walktrought inziale a livello di meccanica

---
+++ Handy_26Aprile/Page.swift
@@ -0,0 +1,15 @@
+//
+//  page.swift
+//  Handy_26Aprile
+//
+//  Created by Federico Cattaneo on 27/04/2017.
+//  Copyright © 2017 Federico Cattaneo. All rights reserved.
+//
+
+import Foundation
+
+struct Page {
+    let title: String
+    let message: String
+    let imageName: String
+}
---
+++ Handy_26Aprile/ViewController.swift
@@ -0,0 +1,116 @@
+//
+//  ViewController.swift
+//  Handy_26Aprile
+//
+//  Created by Federico Cattaneo on 26/04/2017.
+//  Copyright © 2017 Federico Cattaneo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    
+    lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal // da scroll vert. a horiz
+        layout.minimumLineSpacing = 0
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.backgroundColor = UIColor.white
+        cv.dataSource = self
+        cv.delegate = self
+        cv.isPagingEnabled = true //paging
+        return cv
+    }()
+    
+    let cellId = "cellId"
+    
+    let pages: [Page] = {
+        let firstPage = Page(title: "Handy First Page", message: "It's free to send your books to yatta my ass is itcy why don't scratch it", imageName: "page1")
+        
+        let secondPage = Page(title: "Handy Second Page", message: "It's free to send your books to yatta my ass is itcy why don't scratch it", imageName: "page2")
+        
+        let thirdPage = Page(title: "Handy Third Page", message: "It's free to send your books to yatta my ass is itcy why don't scratch it", imageName: "page3")
+        
+        return[firstPage, secondPage, thirdPage]
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.addSubview(collectionView)
+        
+//        collectionView.frame = view.frame
+        collectionView.anchorToTop(view.topAnchor, left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor)
+        
+        
+        collectionView.register(PageCell.self, forCellWithReuseIdentifier: cellId)
+
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return pages.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! PageCell
+        
+        let page = pages[indexPath.item]
+        cell.page = page
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: view.frame.height)
+    }
+}
+
+extension UIView {
+    
+    func anchorToTop(_ top: NSLayoutYAxisAnchor? = nil, left: NSLayoutXAxisAnchor? = nil, bottom: NSLayoutYAxisAnchor? = nil, right: NSLayoutXAxisAnchor? = nil) {
+        
+        anchorWithConstantsToTop(top, left: left, bottom: bottom, right: right, topConstant: 0, leftConstant: 0, bottomConstant: 0, rightConstant: 0)
+    }
+    
+    func anchorWithConstantsToTop(_ top: NSLayoutYAxisAnchor? = nil, left: NSLayoutXAxisAnchor? = nil, bottom: NSLayoutYAxisAnchor? = nil, right: NSLayoutXAxisAnchor? = nil, topConstant: CGFloat = 0, leftConstant: CGFloat = 0, bottomConstant: CGFloat = 0, rightConstant: CGFloat = 0) {
+        
+        _ = anchor(top, left: left, bottom: bottom, right: right, topConstant: topConstant, leftConstant: leftConstant, bottomConstant: bottomConstant, rightConstant: rightConstant)
+    }
+    
+    func anchor(_ top: NSLayoutYAxisAnchor? = nil, left: NSLayoutXAxisAnchor? = nil, bottom: NSLayoutYAxisAnchor? = nil, right: NSLayoutXAxisAnchor? = nil, topConstant: CGFloat = 0, leftConstant: CGFloat = 0, bottomConstant: CGFloat = 0, rightConstant: CGFloat = 0, widthConstant: CGFloat = 0, heightConstant: CGFloat = 0) -> [NSLayoutConstraint]? {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        var anchors = [NSLayoutConstraint]()
+        
+        if let top = top {
+            anchors.append(topAnchor.constraint(equalTo: top, constant: topConstant))
+        }
+        
+        if let left = left {
+            anchors.append(leftAnchor.constraint(equalTo: left, constant: leftConstant))
+        }
+        
+        if let bottom = bottom {
+            anchors.append(bottomAnchor.constraint(equalTo: bottom, constant: -bottomConstant))
+        }
+        
+        if let right = right {
+            anchors.append(rightAnchor.constraint(equalTo: right, constant: -rightConstant))
+        }
+        
+        if widthConstant > 0 {
+            anchors.append(widthAnchor.constraint(equalToConstant: widthConstant))
+        }
+        
+        if heightConstant > 0 {
+            anchors.append(heightAnchor.constraint(equalToConstant: heightConstant))
+        }
+        
+        anchors.forEach({$0.isActive = true})
+        
+        return anchors
+    }
+    
+}
+
+
